4c855d2271172dc1f923317626c0621a4b0ddde7
Shedrack Sunday <Hannania002@gmail.com>
2024-11-04 21:15:52 +0100

    Add two sum solution with hash solution

---
+++ patterns/01-two-pointers/two-sum/solution.py
@@ -0,0 +1,88 @@
+"""
+1. Two Sum
+https://leetcode.com/problems/two-sum/
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+"""
+
+## Approach 1: Brute Force
+Algorithm
+
+The brute force approach is simple. Loop through each element x and find if there is another value that equals to target−x.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+        # Return an empty list if no solution is found
+        return []
+    
+    
+### Complexity Analysis
+
+Time complexity: O(n^2).
+For each element, we try to find its complement by looping through the rest of the array which takes O(n) time. Therefore, the time complexity is O(n^2).
+
+Space complexity: O(1).
+The space required does not depend on the size of the input array, so only constant space is used.
+
+## Approach 2: One-pass Hash Table
+Algorithm
+
+It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table, we also look back to check if current element's complement already exists in the hash table. If it exists, we have found a solution and return the indices immediately.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+        # Return an empty list if no solution is found
+        return []
+        
+### Complexity Analysis
+
+Time complexity: O(n).
+We traverse the list containing n elements only once. Each lookup in the table costs only O(1) time.
+
+Space complexity: O(n).
+The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
+
+## Learnings:
+- The brute force approach is simple but time complexity is O(n^2)
+- The one-pass hash table approach is more efficient with time complexity O(n) and space complexity O(n)
